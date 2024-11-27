c1d00dbeadd4dc8604e61297256047a817c4fe1a
Vladimir Vinageras <vvinageras@gmail.com>
2024-11-28 01:14:49 +0200

    Initial Commit

---
+++ WorkshopShoppingList/WorkshopShoppingListApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkshopShoppingListApp.swift
+//  WorkshopShoppingList
+//
+//  Created by Vladimir Vinakheras on 28.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct WorkshopShoppingListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
