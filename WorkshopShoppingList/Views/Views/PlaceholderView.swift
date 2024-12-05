0981be056f6e2b5716ecaf04524ebd57ff988627
Vladimir Vinageras <vvinageras@gmail.com>
2024-12-05 02:58:33 +0200

    SplashScreen created

---
+++ WorkshopShoppingList/Views/Views/PlaceholderView.swift
@@ -0,0 +1,18 @@
+//
+//  PlaceholderView.swift
+//  WorkshopShoppingList
+//
+//  Created by Vladimir Vinakheras on 05.12.2024.
+//
+
+import SwiftUI
+
+struct PlaceholderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PlaceholderView()
+}
